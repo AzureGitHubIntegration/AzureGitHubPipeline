11dcfaf74dbe594c9c005fff51b9e0fc89dd4935
Loganayaki Kuppusamy <loganayaki.kuppusamy@slalom.com>
2021-09-17 15:11:23 -0400

    test:

---
+++ src/main/java/com/AzureGitHubPipeline/AzureGitHubPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.AzureGitHubPipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureGitHubPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureGitHubPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AzureGitHubPipeline/AzureGitHubPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AzureGitHubPipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureGitHubPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
